f7c4ea05372c6a89d31fdc0dc9043759a0155655
thanos stam <th.stamod@gmail.com>
2020-06-27 08:59:18 +0300

    remove some logic from component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,77 +1,65 @@
-import React, { useState, useEffect, useRef } from "react"
-import Card from "./Card"
-import letters from "../letters"
-import { getLetter, findEmptyPosition } from "../utils"
+import React, { useState, useEffect, useRef } from "react";
+import Card from "./Card";
+import setRandomPositions from "../utils";
 
 const Board = ({ size, availableMoves }) => {
-  const [state, setState] = useState({})
-  const [initialOpen, setInitialOpen] = useState(true)
-  const [open, setOpen] = useState(null)
-  let p2 = useRef(null)
-  const lettersSet = useRef(new Set())
-  let [tries, setTries] = useState(0)
-  let [remaining, setRemaining] = useState(Math.pow(size, 2) / 2)
+  const [state, setState] = useState({});
+  const [initialOpen, setInitialOpen] = useState(true);
+  const [open, setOpen] = useState(null);
+  let p2 = useRef(null);
+  const lettersSet = useRef(new Set());
+  let [tries, setTries] = useState(0);
+  let [remaining, setRemaining] = useState(Math.pow(size, 2) / 2);
 
   useEffect(() => {
-    const max = Math.pow(size, 2)
-    const tmpArr = new Array(max).fill(null)
-    const s = {}
-    for (let i = 0; i < max / 2; i++) {
-      const letter = getLetter(letters, lettersSet)
-      const q = findEmptyPosition(tmpArr, max)
-      tmpArr[q] = letter
-      s[q] = { val: letter, popen: false }
-      const p = findEmptyPosition(tmpArr, max)
-      tmpArr[p] = letter
-      s[p] = { val: letter, popen: false }
-    }
-    setState(s)
+    const s = setRandomPositions(size, lettersSet);
+    setState(s);
 
     setTimeout(() => {
-      setInitialOpen(false)
-    }, 2000)
+      setInitialOpen(false);
+    }, 2000);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, []);
 
   const selectCard = (selectedCard) => {
-    if (initialOpen) return
+    if (initialOpen) return;
 
-    if (availableMoves !== 0 && availableMoves < tries + remaining) return
+    if (availableMoves !== 0 && availableMoves < tries + remaining) return;
 
-    if (open && p2.current) return
+    if (open && p2.current) return;
 
     if (!open) {
-      setOpen(selectedCard)
-      return
+      setOpen(selectedCard);
+      return;
     }
     if (open && !p2.current) {
-      p2.current = selectedCard
-      setTries(++tries)
+      p2.current = selectedCard;
+      setTries(++tries);
     }
     if (state[open]?.val !== state[p2.current].val) {
       setTimeout(() => {
-        p2.current = null
-        setOpen(null)
-      }, 2000)
+        p2.current = null;
+        setOpen(null);
+      }, 2000);
 
-      return
+      return;
     }
     if (state[open]?.val === state[p2.current].val) {
       setState({
         ...state,
         [open]: { ...state[open], popen: true },
         [p2.current]: { ...state[p2.current], popen: true },
-      })
-      setOpen(null)
-      p2.current = null
-      setRemaining(--remaining)
+      });
+      setOpen(null);
+      p2.current = null;
+      setRemaining(--remaining);
     }
-  }
+  };
 
   const getCards = (_cards) => {
-    const last = []
-    let cards = []
-    let index = 0
+    const last = [];
+    let cards = [];
+    let index = 0;
     for (let [key, value] of Object.entries(_cards)) {
       cards.push(
         <Card
@@ -84,20 +72,20 @@ const Board = ({ size, availableMoves }) => {
               : false
           }
         />
-      )
-      index++
+      );
+      index++;
       if (size < 8 && index % size === 0) {
         last.push(
           <div className="flexRow" key={index}>
             {cards}
           </div>
-        )
-        cards = []
+        );
+        cards = [];
       }
     }
-    return size < 8 ? last : cards
-  }
-  const container = "container " + (size < 8 ? "container--strict" : "")
+    return size < 8 ? last : cards;
+  };
+  const container = "container " + (size < 8 ? "container--strict" : "");
   return (
     <>
       <div className={container}>{getCards(state)}</div>
@@ -111,7 +99,7 @@ const Board = ({ size, availableMoves }) => {
         </span>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Board
+export default Board;
--- src/utils.js
+++ src/utils.js
@@ -1,26 +1,46 @@
-export const getLetter = (letters, set) => {
-  const S = 26
-  const setSize = set.current.size
-  if (setSize < 26) {
-    set.current.add(letters[setSize])
-    return letters[setSize]
-  }
-  const major = parseInt(setSize / S)
-  let l = letters[major - 1]
-  const minor = setSize - S * major
-  l += letters[minor]
-  set.current.add(l)
-  return l
-}
+import letters from "./letters";
 
-export const findEmptyPosition = (tmpArr, max) => {
-  let rn = getRandomNumber(max)
-  if (tmpArr[rn] !== null) {
-    rn = findEmptyPosition(tmpArr, max)
+const getLetter = (letters, set) => {
+  const S = 26;
+  const setSize = set.current.size;
+  if (setSize < 26) {
+    set.current.add(letters[setSize]);
+    return letters[setSize];
   }
-  return rn
-}
+  const major = parseInt(setSize / S);
+  let l = letters[major - 1];
+  const minor = setSize - S * major;
+  l += letters[minor];
+  set.current.add(l);
+  return l;
+};
+
+const findEmptyPosition = (tmpArr, max) => {
+  let rn = getRandomNumber(max);
+  if (tmpArr[rn] !== null) {
+    rn = findEmptyPosition(tmpArr, max);
+  }
+  return rn;
+};
 
 const getRandomNumber = (max) => {
-  return Math.floor(Math.random() * max)
-}
+  return Math.floor(Math.random() * max);
+};
+
+const setRandomPositions = (size, lettersSet) => {
+  const max = Math.pow(size, 2);
+  const tmpArr = new Array(max).fill(null);
+  const s = {};
+  for (let i = 0; i < max / 2; i++) {
+    const letter = getLetter(letters, lettersSet);
+    const q = findEmptyPosition(tmpArr, max);
+    tmpArr[q] = letter;
+    s[q] = { val: letter, popen: false };
+    const p = findEmptyPosition(tmpArr, max);
+    tmpArr[p] = letter;
+    s[p] = { val: letter, popen: false };
+  }
+  return s;
+};
+
+export default setRandomPositions;
